879418a72f9834e0bdfb1187293b2b230ad51ab2
mao <1296193245@qq.com>
2022-10-29 14:10:16 +0800

    hibernate_validator_demo2

--- use/src/main/java/mao/use/entity/User.java
+++ use/src/main/java/mao/use/entity/User.java
@@ -43,5 +43,43 @@ public class User
     @Pattern(regexp = "[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\\\\.[a-zA-Z0-9_-]+)+$", message = "邮箱格式不正确")
     private String email;
 
+    public Integer getId()
+    {
+        return id;
+    }
 
+    public void setId(Integer id)
+    {
+        this.id = id;
+    }
+
+    public String getUserName()
+    {
+        return userName;
+    }
+
+    public void setUserName(String userName)
+    {
+        this.userName = userName;
+    }
+
+    public Integer getAge()
+    {
+        return age;
+    }
+
+    public void setAge(Integer age)
+    {
+        this.age = age;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
 }
